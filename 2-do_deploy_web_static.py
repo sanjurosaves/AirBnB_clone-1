d166f41d5ceaebf4baaca7dd2af885e008037bd0
Alex Allen <325@holbertonschool.com>
2018-07-30 20:56:01 +0000

    correct for pep8 and remove nginx restart

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,7 +9,8 @@ from fabric.context_managers import cd
 from fabric.api import env
 from datetime import datetime
 
-env.hosts = ['root@34.206.234.184:39409']
+env.hosts = ['35.237.92.195', '35.237.197.1']
+
 
 def do_pack():
     """ generates a .tgz archive from the contentes of web_static folder """
@@ -22,6 +23,7 @@ def do_pack():
     except:
         return None
 
+
 def do_deploy(archive_path):
     if archive_path is None:
         return False
@@ -30,22 +32,22 @@ def do_deploy(archive_path):
         put(archive_path, '/tmp/')
         # uncompress
         archive_name = archive_path[9:-4]
-        print(archive_name)
-        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
-        run('tar -xzf /tmp/{}.tgz -C '
-            '/data/web_static/releases/{}'
-            .format(archive_name, archive_name))
+        sudo('mkdir -p /data/web_static/releases/{}'.format(archive_name))
+        sudo('tar -xzf /tmp/{}.tgz -C '
+             '/data/web_static/releases/{}'
+             .format(archive_name, archive_name))
         # delete archive
         run('rm /tmp/{}.tgz'.format(archive_name))
-        run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(archive_name, archive_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(archive_name))
+        sudo('mv -f /data/web_static/releases/{}/web_static/* '
+             '/data/web_static/releases/{}/'.format(archive_name,
+                                                    archive_name))
+        sudo('rm -rf /data/web_static/releases/{}/web_static'
+             .format(archive_name))
         # delete symbolic link
-        run('rm -rf /data/web_static/current')
+        sudo('rm -rf /data/web_static/current')
         # create new symbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
-            format(archive_name))
+        sudo('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
+             format(archive_name))
         print("New version deployed!")
     except:
         return False
