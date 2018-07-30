58a5a76e006be409e9622b3ae3229be330e4a788
Alex Allen <325@holbertonschool.com>
2018-07-30 19:58:49 +0000

    remove versions direct from archive_name

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,23 +29,23 @@ def do_deploy(archive_path):
         # upload archive (put) to tmp dir of webserver
         put(archive_path, '/tmp/')
         # uncompress
-        archive_path = archive_path[:-4]
+        archive_name = archive_path[-9:-4]
         print(archive_path)
-        run('mkdir -p /data/web_static/releases/{}'.format(archive_path))
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_name))
         run('tar -xzf /tmp/{}.tgz -C '
             '/data/web_static/releases/{}'
-            .format(archive_path, archive_path))
+            .format(archive_name, archive_name))
         # delete archive
-        run('rm /tmp/{}.tgz'.format(archive_path))
+        run('rm /tmp/{}.tgz'.format(archive_name))
         run('mv /data/web_static/releases/{}/web_static/* '
-            '/data/web_static/releases/{}/'.format(archive_path, archive_path))
+            '/data/web_static/releases/{}/'.format(archive_name, archive_name))
         run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(archive_path))
+            .format(archive_name))
         # delete symbolic link
         run('rm -rf /data/web_static/current')
         # create new symbolic link
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.
-            format(archive_path))
+            format(archive_name))
         print("New version deployed!")
     except:
         return False
