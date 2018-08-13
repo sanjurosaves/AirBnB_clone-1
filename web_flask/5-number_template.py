b7f84ba289574cada7c5dae419c4e8843da8210d
Alex Allen <325@holbertonschool.com>
2018-08-13 20:32:09 +0000

    import render_template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ starts a Flask web application """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
