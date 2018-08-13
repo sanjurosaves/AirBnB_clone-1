b6dcc720757114a407b8f5d4286fbfd5a22300aa
Alex Allen <325@holbertonschool.com>
2018-08-13 19:09:53 +0000

    add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    app.strict_slashes=False
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    app.strict_slashes=False
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c():
+    app.strict_slashes=False
+    return 'C {}'.format(text.replace('_', ' ')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
