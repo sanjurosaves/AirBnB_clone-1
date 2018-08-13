2472d91fc21e33673f758ac724e10c62f8a135d0
Alex Allen <325@holbertonschool.com>
2018-08-13 18:22:56 +0000

    initial commit for flask project

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    app.strict_slashes = False
+    return 'Hello HBNB!'
---
+++ web_flask/templates/README.md
@@ -0,0 +1,10 @@
+# Jinja Templates
+
+File | Description
+--- | ---
+5-number.html |
+6-number_odd_or_even.html |
+7-states_list.html |
+8-cities_by_states.html |
+9-states.html |
+10-hbnb_filters.html |
