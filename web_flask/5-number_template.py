4a7e9625976a50aefc6b2ae3eab4ec16a0dbc14a
Alex Allen <325@holbertonschool.com>
2018-08-13 20:22:37 +0000

    add forgotten leading slash

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -31,7 +31,7 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('number_template/<int:n>')
+@app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', n=n)
 
